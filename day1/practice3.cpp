fd32a0786bc9d80629d98a02ddb9ee318b0f439d
Levi <126132428+AluminumShark@users.noreply.github.com>
2024-07-07 18:59:23 +0800

    adjust files

---
+++ day1/practice3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1 = 0, num2 = 0;
+    
+    cout << "Please enter two numbers, seprated by a white space or enter: ";
+    cin >> num1 >> num2;
+
+    cout << "The sum is " << num1 + num2;
+
+    return 0;
+}
---
+++ day1/pratice5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1 = 0, num2 = 0;
+    cout << "Please enter two numbers,"
+        << "separated by a shite space or enter: ";
+    cin >> num1 >> num2;
+
+    while (num1 > num2)
+    {
+        cout << "number 1 is " << num1 << "\n";
+        num1 = num1 - 1;
+    }
+
+    return 0;
+    
+}
