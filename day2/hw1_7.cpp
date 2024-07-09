c435ba317af6842449fed5d207cfc7dd4c5dffa4
Levi <126132428+AluminumShark@users.noreply.github.com>
2024-07-09 18:52:15 +0800

    Update hw1_7.cpp

--- day2/hw1_7.cpp
+++ day2/hw1_7.cpp
@@ -3,8 +3,16 @@ using namespace std;
 
 int main()
 {
-    int baseDemand = 0, priceSensitivity = 0, unitPrice = 0, unitCost = 0;
+    int baseDemand = 0, priceSensitivity = 0, unitCost = 0;
+    int unitPrice = 0, maxProfit = 0;
 
-    
+    cin >> baseDemand >> priceSensitivity >> unitCost;
 
+    unitPrice = (baseDemand + priceSensitivity * unitCost) / (2 * priceSensitivity);
+
+    maxProfit = (baseDemand - priceSensitivity * unitPrice) * (unitPrice - unitCost);
+
+    cout << unitPrice << "," << maxProfit;
+
+    return 0;
 }
